5b93fa17a2bad4e5dc269d96f097d04b7ed5f73a
Karl Hallsby <karl@hallsby.com>
2026-02-12 20:29:37 -0600

    Build a zipapp of FireSlurm for running the FireSim simulation
    We need to be able to access FireSlurm's run.py features to run the
    simulation for us. The easiest way to make sure that this is
    compatible across many machines is to just bundle all of FireSlurm up
    into a fireslurm.pyz. Then this PYthon Zip has a single entry-point,
    the run() function we are interested.

--- fireslurm/batch.py
+++ fireslurm/batch.py
@@ -15,7 +15,7 @@ from dataclasses import dataclass
 import subprocess
 import re
 import textwrap
-import shutil
+import zipapp
 
 import fireslurm.utils as utils
 
@@ -33,6 +33,49 @@ class JobInfo:
     """The numerical ID Slurm assigned this job."""
 
 
+def build_job_run_py(target) -> Path:
+    """
+    Build a submission zip of FireSlurm to run FireSim.
+    Returns the path to the zipapp.
+
+    This is a Python zipapp that does not use any 3rd-party dependencies, so you
+    should be able to directly execute this on pretty much any machine,
+    particularly any Cheese machine.
+    """
+
+    def exclude_dirs(to_include: Path) -> bool:
+        """
+        Determine if the path TO_INCLUDE should be included.
+        Returns True if TO_INCLUDE should be included.
+        """
+        EXCLUDE_DIRS = [
+            ".git",
+            ".direnv",
+            "__pycache__",
+            ".ruff_cache",
+            ".venv",
+        ]
+        should_include = not any(map(lambda exclude: exclude in str(to_include), EXCLUDE_DIRS))
+        logger.debug(f"Should include {to_include}? {should_include}")
+        return should_include
+
+    # Grab the root of FireSlurm by walking 2 levels up from run.py.
+    # The structure is fireslurm/fireslurm/run.py, so parents[1] brings us up to
+    # the project root (fireslurm), which we can then pack together with zipapp.
+    import fireslurm.run
+
+    BATCH_SCRIPT = Path(inspect.getfile(fireslurm.run))
+    FIRESLURM = BATCH_SCRIPT.parents[1]
+
+    zipapp.create_archive(
+        FIRESLURM,
+        target,
+        interpreter="/usr/bin/env python3",
+        main="fireslurm.__main__:main",
+        filter=exclude_dirs,
+        compressed=True,
+    )
+    return target
 
 
 def build_job_script_contents(
@@ -54,7 +97,6 @@ def build_job_script_contents(
 
     verbose_flag = "-" + "v" * verbosity if verbosity > 0 else ""
 
-    run_py = Path("/tank/karl/buoyancy/firesim-slurm/run.py")
     return textwrap.dedent(f"""\
     #!/usr/bin/env bash
     echo "Hello from $SLURM_JOB_ID"
@@ -90,10 +132,10 @@ def build_sbatch_script(
     results_dir.mkdir(parents=True, exist_ok=True)
 
     script = (config_dir / f"run-{run_name}.sh").resolve()
-    # TODO: We need to robustify the finding of run.py!
-    OG_RUN_PY = Path("/tank/karl/buoyancy/firesim-slurm/run.py")
-    shutil.copy(OG_RUN_PY, config_dir)
-    job_run_py = config_dir / "run.py"
+    job_run_py = build_job_run_py(config_dir / "fireslurm.pyz")
+    assert job_run_py and job_run_py.exists(), (
+        f"FireSlurm's runner must be in {config_dir.resolve()}"
+    )
     logger.info(f"Writing the sbatch submission to {script=!s}")
     with open(script, "w") as s:
         s.write(
