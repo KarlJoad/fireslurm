0f7af84e2bd3bf3cc60862fe9d43b749bd2b79a5
Karl Hallsby <karl@hallsby.com>
2026-02-24 19:11:18 -0600

    Slurm JobInfo dataclasses track the FireSlurm ID that caused the run

--- src/fireslurm/slurm.py
+++ src/fireslurm/slurm.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import fireslurm.config as config
 
 
 @dataclass(frozen=True)
@@ -9,3 +10,10 @@ class JobInfo:
 
     slurm_job_id: int = -1
     """The numerical ID Slurm assigned this job."""
+
+    run_id: config.FireSlurmID = config.DEFAULT_FIRESLURM_ID
+    """
+    The FireSlurm ID assigned to this Slurm job because FireSlurm controls
+    the job.
+    This is linked to the FireSlurmConfig that is used to launch the Slurm job.
+    """
