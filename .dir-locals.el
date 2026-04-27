5a6141de508b929e83f4ea5e01ae4dc7932c7a7d
Karl Hallsby <karl@hallsby.com>
2026-04-26 21:51:43 -0500

    Enable eglot configuration now that rassumfrassum is provided by flake.nix

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((python-mode
+  . ((eglot-server-programs
+      . ((python-mode python-ts-mode) . "rass python"))))
+ (python-ts-mode
+  . ((eglot-server-programs
+      . ((python-mode python-ts-mode) . "rass python"))))
+ )
