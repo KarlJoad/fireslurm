5f70e270d1610300f9e5052eda5b8101fd7497f7
Karl Hallsby <karl@hallsby.com>
2026-05-01 23:56:56 -0500

    Properly wrap the FireSlurmID types at construction-time

--- src/fireslurm/config.py
+++ src/fireslurm/config.py
@@ -23,9 +23,11 @@ FireSlurmID = NewType("FireSlurmID", uuid.UUID)
 # to 0. Python 3.14+ has this defined as a special value. We must construct it
 # ourselved on older Pythons.
 if sys.version_info < (3, 14):
-    DEFAULT_FIRESLURM_ID: FireSlurmID = uuid.UUID("00000000-0000-0000-0000-000000000000")
+    DEFAULT_FIRESLURM_ID: FireSlurmID = FireSlurmID(
+        uuid.UUID("00000000-0000-0000-0000-000000000000")
+    )
 else:
-    DEFAULT_FIRESLURM_ID: FireSlurmID = uuid.NIL
+    DEFAULT_FIRESLURM_ID: FireSlurmID = FireSlurmID(uuid.NIL)
 
 
 def _run_uuid() -> FireSlurmID:
@@ -36,9 +38,9 @@ def _run_uuid() -> FireSlurmID:
     """
     f = None
     if sys.version_info < (3, 14):
-        f = uuid.uuid4()
+        f = FireSlurmID(uuid.uuid4())
     else:
-        f = uuid.uuid7()
+        f = FireSlurmID(uuid.uuid7())
     assert f is not None, "You must have a UUID function to generate run IDs"
     return f
 
