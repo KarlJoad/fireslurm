fb05ceee02ce39adce4c47fe1b325bece34dd827
Karl Hallsby <karl@hallsby.com>
2026-05-01 23:25:48 -0500

    Include additional logging on file validation checks

--- src/fireslurm/validation.py
+++ src/fireslurm/validation.py
@@ -14,13 +14,16 @@ def path_is_readable_dir(dir: Path) -> bool:
     multiple processes/threads accessing/working with these files!
     """
     logger.debug(f"Validating that {dir=!s} is a readable directory!")
-    return all(
+    readable_dir = all(
         [
             dir.exists(),
             dir.is_dir(),
             os.access(dir, os.R_OK),
         ]
     )
+    if not readable_dir:
+        logger.error(f"{dir=!s} is not a readable path!")
+    return readable_dir
 
 
 def path_is_writable_dir(dir: Path) -> bool:
@@ -34,13 +37,16 @@ def path_is_writable_dir(dir: Path) -> bool:
     multiple processes/threads accessing/working with these files!
     """
     logger.debug(f"Validating that {dir=!s} is a writable directory!")
-    return all(
+    writable_dir = all(
         [
             dir.exists(),
             dir.is_dir(),
             os.access(dir, os.W_OK),
         ]
     )
+    if not writable_dir:
+        logger.error(f"{dir=!s} is not a writable path!")
+    return writable_dir
 
 
 def path_is_readable_file(f: Path) -> bool:
@@ -51,13 +57,16 @@ def path_is_readable_file(f: Path) -> bool:
     multiple processes/threads accessing/working with these files!
     """
     logger.debug(f"Validating that {f=!s} is a readable file!")
-    return all(
+    readable_file = all(
         [
             f.exists(),
             f.is_file(),
             os.access(f, os.R_OK),
         ]
     )
+    if not readable_file:
+        logger.error(f"{f=!s} is not a readable file!")
+    return readable_file
 
 
 def path_is_executable_file(f: Path) -> bool:
